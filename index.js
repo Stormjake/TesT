6c7869b9a678acd085cfda670028e2aa39bc1f80
Stormjake <jokerbad418@gmail.com>
2024-12-24 17:24:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ console.log('Bot connected to whatsapp ✅')
 
 let up = `ALIEN-X connected successful ✅\n\nPREFIX: ${prefix}`;
 
-conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: https://i.ibb.co/KrWVCH1/FB-IMG-17250502056703306.jpg }, caption: up })
+conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://i.imgur.com/mYXK0Jn.jpeg` }, caption: up })up })
 
 }
 })
